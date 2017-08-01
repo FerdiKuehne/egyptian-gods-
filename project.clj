d2459617b65419e51b728a2fa2836ebd35d63f05
FerdiKuehne <ferdi.kuehne@gmx.net>
2017-08-01 16:39:46 +0200

    update

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]
                  [compojure "1.1.8"]
-                 [ring/ring-json "0.4.0"]])
+                 [ring/ring-defaults "0.3.1"]
+                 [ring/ring-json "0.4.0"]
+                 [pdfboxing "0.1.13"] ])
--- src/egyptian_gods/core.clj
+++ src/egyptian_gods/core.clj
@@ -1,13 +1,32 @@
 (ns egyptian-gods.core
   (:require [compojure.core :refer :all]
             [org.httpkit.server :refer :all]
-            [ring.util.response :as resp] ))
+            [ring.util.response :as resp]
+            [ring.middleware.defaults :refer :all]
+            [compojure.route :as route]
+            [pdfboxing.text :as text]
+            [pdfboxing.info :as info]
+            [clojure.string :as str]))
+
+(def port {:port 3000})
+
+(def pdf "egyptgods.pdf")
+
+(def listofGods (text/extract pdf))
+
+(str/split listofGods #"\n")
 
 (defroutes app-routes
   (GET "/" [] (resp/file-response "index.html" {:root "resources/public"}))
-  (GET "/list-of-gods" [] (resp/file-response "index.html" {:root "resources/public"})))
+  (GET "/list-of-gods" [] (resp/file-response "list.html" {:root "resources/public"}))
+  (GET "/app-version" req (str "Hello World v" (:app-version req)))
+  (GET "/pdf" [] listofGods)
+    (route/resources "/"))
+
+(defn wrap-version [handler]
+  (fn [request]
+      (handler (assoc request :app-version "1.0.1"))))
 
 (defn -main []
-  (run-server app-routes {:port 3000}))
-
-
+  (run-server app-routes port)
+  (println "server online at port:" (-> port :port)))
